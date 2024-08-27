2a665caaf04f186df8fc50da0a1a2cb2c1ae21fa
Vicente Quiroz <99621vicenteaqv@gmail.com>
2024-08-26 23:11:21 -0400

    Se creo la logica pero no funciona correctamente aun!

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 'use strict'
 
-const inputX = document.getElementById("input-x")
-const inputY = document.getElementById("input-y")
-const inputValueX = inputX.value;
-const inputValueY = inputY.value;
+const enviar = document.getElementById('enviar');
+enviar.onclick(dividirNumeros());
 
-function convertData(inputValueX, inputValueY){
-    if(inputValueX && inputValueY == isNaN(inputValueX) && isNaN(inputValueY)){
-        inputValueX /= 8;
-        inputValueY /= 8;
-        console.log(inputValueX, inputValueY)
-    }else{
-        console.log("no es un numero")
+function dividirNumeros(){
+    let inputX = document.getElementById("input-x").value
+    let inputY = document.getElementById("input-y").value
+    
+    if(isNaN(inputX) || isNaN(inputY)){
+        alert("Por favor, ingresa solo numeros en ambos campos.");
+        return;
+    }else if(inputX == undefined || inputY == undefined){
+        alert("No has definido una coordenada.")
+        return;
     }
+
+    const num1 = parseFloat(inputX);
+    const num2 = parseFloat(inputY);
+
+    const resultado1 = num1 / 8;
+    const resultado2 = num2 / 8;
+
+    const divReturnData = document.getElementById('return-data');
+    divReturnData.classList.add('result');
+    
+    const lista = document.createElement('ul');
+    const elementList = document.createElement('li');
+    elementList.textContent = `X ${resultado1}  Y ${resultado2}`;
+    lista.appendChild(elementList);
+
+    divReturnData.appendChild(lista);
 }
